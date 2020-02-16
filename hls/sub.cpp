bc6d715eaca968494f26cc4b600d1599253c8fa5
royforu <44690084+royforu@users.noreply.github.com>
2020-02-16 20:59:47 +0800

    Add files via upload

---
+++ hls/sub.cpp
@@ -0,0 +1,8 @@
+void sub(int a, int b, int& c) {
+#pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS INTERFACE s_axilite port=a
+#pragma HLS INTERFACE s_axilite port=b
+#pragma HLS INTERFACE s_axilite port=c
+
+    c = a - b;
+}
